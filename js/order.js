5a6e5effdf295adc87de96d983466f03d07bd589
diogofsvilela <diogofsvilela@gmail.com>
2017-04-03 10:42:42 +0100

    Add of atributes and methods to order object

--- js/order.js
+++ js/order.js
@@ -1,11 +1,41 @@
 // Order Object
 
-function Order() {
+function Order(product) {
     this.creationTime = $.now();
     this.status = "Order Placed";
+    this.product = product;
+    this.classification = 0;
 
     this.getStatus = function () {
         return this.status;
     }
-}
 
+    this.setStatus = function (status) {
+        // SEE IF THE ARGUMENT IS VALID
+        if (true) {
+            this.status = status;
+        }
+    }
+
+    this.getRemainingTime = function () {
+        if (this.status == "Served") {
+            return 0;         
+        }
+        else {
+            var diff = this.creationTime - $.now;
+            return Math.floor(diff / 1000);
+        }
+    }
+
+    this.getClassification = function () {
+        return this.classification;
+    }
+
+    this.setClassification = function (classification) {
+        if (Number.isInteger(classification)) {
+            if (classification > 0 && classification <= 5) {
+                this.classification = classification;
+            }
+        }
+    }
+}
