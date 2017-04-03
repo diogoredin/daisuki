3f7570778c0e53a5e0e5cfd43c2c7b91b0412be5
Diogo Redin <diogo.redin@gmail.com>
2017-04-03 19:54:10 +0100

    HTML pages for everyone yaaaayyyy

--- js/navigation/new_order.js
+++ js/navigation/new_order.js
@@ -6,11 +6,13 @@
 
 $(document).ready(function() {
 
-	$(".restaurant_products").mCustomScrollbar();
+	var depth_one;
+	var depth_two;
 
 	// Category 1
 	$(document).on('click', "#restaurant_category1", function(e) {
-		$('#page').load("order_category.html", function(){
+		depth_one = $("#page").clone();
+		$('#page').load("categories/category1.html", function(){
         	$('#screen_order_category').fadeIn(1000);
     	});
 		e.preventDefault();
@@ -18,7 +20,8 @@ $(document).ready(function() {
 
 	// Category 2
 	$(document).on('click', "#restaurant_category2", function(e) {
-		$('#page').load("order_category.html", function(){
+		depth_one = $("#page").clone();
+		$('#page').load("categories/category2.html", function(){
         	$('#screen_order_category').fadeIn(1000);
     	});
 		e.preventDefault();
@@ -26,10 +29,82 @@ $(document).ready(function() {
 
 	// Category 3
 	$(document).on('click', "#restaurant_category3", function(e) {
-		$('#page').load("order_category.html", function(){
+		depth_one = $("#page").clone();
+		$('#page').load("categories/category3.html", function(){
         	$('#screen_order_category').fadeIn(1000);
     	});
 		e.preventDefault();
+	});
+
+	// Product 1
+	$(document).on('click', "#product1", function(e) {
+		depth_two = $("#page").clone();
+		$('#page').load("products/product1.html", function(){
+        	$('#screen_product').fadeIn(1000);
+    	});
+		e.preventDefault();
+	});
+
+	// Product 2
+	$(document).on('click', "#product2", function(e) {
+		depth_two = $("#page").clone();
+		$('#page').load("products/product2.html", function(){
+        	$('#screen_product').fadeIn(1000);
+    	});
+		e.preventDefault();
+	});
+
+	// Product 3
+	$(document).on('click', "#product3", function(e) {
+		depth_two = $("#page").clone();
+		$('#page').load("products/product3.html", function(){
+        	$('#screen_product').fadeIn(1000);
+    	});
+		e.preventDefault();
+	});
+
+	// Product 4
+	$(document).on('click', "#product4", function(e) {
+		depth_two = $("#page").clone();
+		$('#page').load("products/product4.html", function(){
+        	$('#screen_product').fadeIn(1000);
+    	});
+		e.preventDefault();
+	});
+
+	// Product 5
+	$(document).on('click', "#product5", function(e) {
+		depth_two = $("#page").clone();
+		$('#page').load("products/product5.html", function(){
+        	$('#screen_product').fadeIn(1000);
+    	});
+		e.preventDefault();
+	});
+
+	// Product 6
+	$(document).on('click', "#product6", function(e) {
+		depth_two = $("#page").clone();
+		$('#page').load("products/product6.html", function(){
+        	$('#screen_product').fadeIn(1000);
+    	});
+		e.preventDefault();
+	});
+
+	// Product 7
+	$(document).on('click', "#product7", function(e) {
+		depth_two = $("#page").clone();
+		$('#page').load("products/product7.html", function(){
+        	$('#screen_product').fadeIn(1000);
+    	});
+		e.preventDefault();
+	});
+
+	$(document).on('click', "#screen_order_category .back_button", function(e) {
+		$("#page").replaceWith(depth_one);
+	});
+
+	$(document).on('click', "#screen_product .back_button", function(e) {
+		$("#page").replaceWith(depth_two);
 	});
 
 });
