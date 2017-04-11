90b3e0771bad4e15b6ad24b7db3c6dbcb56769aa
diogofsvilela <diogofsvilela@gmail.com>
2017-04-11 01:43:37 +0100

    my_order_view is dinamic now

--- js/my_order_views.js
+++ js/my_order_views.js
@@ -6,28 +6,44 @@ function loadCurrentOrder() {
 
     for (i = 0; i < NoOrders; i++) {
         var properties = JSON.parse(sessionStorage.getItem(i));
-        var lenght = properties.lenght;
         var state = parseInt(properties[5]);
         if (state == 0) {
-            var categoryName = properties[1];
+            var categoryTag = properties[1];
             var productId = properties[0];
             var product;
+            var categoryName;
 
 
-            switch (categoryName) {
+            switch (categoryTag) {
                 case "maincourses":
                     product = products[0].maincourses[productId];
+                    categoryName = "Main Courses";
                     break;
                 case "drinks":
                     product = products[1].drinks[productId];
+                    categoryName = "Drinks";
                     break;
                 case "deserts":
                     product = products[2].deserts[productId];
+                    categoryName = "Deserts";
                     break;
-            }            
-        
-        
+            }
 
+            $('ul.order_products').append('<li><img src="data/images/' + product.photo + '">\
+                                                <p>Category: ' + categoryName + '</p>\
+                                                <p>Name: ' + product.name + '</p>\
+                                                <p>Price: ' + Number(product.price).toFixed(2) + '</p>\
+                                                <button class="remove_request ' + i.toString() + '">Remove</button></li>');
         }
     }
 }
+
+$(document).ready(function() {
+    $(document).on('click', "button.remove_request", function(e) {
+        var requestId = $( this ).attr('class').split(' ')[1];
+        var request = JSON.parse(sessionStorage.getItem(requestId));
+        request[5] = -1;
+        sessionStorage.setItem(requestId, JSON.stringify(request));
+        e.target.parentNode.parentNode.removeChild(e.target.parentNode);
+    });
+});
