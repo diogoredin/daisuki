38a57d0e21bd3365beef3b978df365f5458d9a0f
Diogo Redin <diogo.redin@gmail.com>
2017-04-11 06:15:24 +0100

    Added styles for the order views

--- js/my_order_views.js
+++ js/my_order_views.js
@@ -5,41 +5,53 @@ function loadCurrentOrder() {
     var products = JSON.parse(sessionStorage.getItem('products'));
 
     for (i = 0; i < NoOrders; i++) {
+
         var properties = JSON.parse(sessionStorage.getItem(i));
         var state = parseInt(properties[5]);
+
         if (state == 0) {
             var categoryTag = properties[1];
             var productId = properties[0];
             var product;
-            var categoryName;
+            var categoryName, categoryTag;
 
 
             switch (categoryTag) {
                 case "maincourses":
                     product = products[0].maincourses[productId];
                     categoryName = "Main Courses";
+                    categoryTag = "maincourses";
                     break;
                 case "drinks":
                     product = products[1].drinks[productId];
                     categoryName = "Drinks";
+                    categoryTag = "drinks";
                     break;
                 case "deserts":
                     product = products[2].deserts[productId];
                     categoryName = "Deserts";
+                    categoryTag = "deserts";
                     break;
             }
 
-            $('ul.order_products').append('<li><img src="data/images/' + product.photo + '">\
-                                                <p>Category: ' + categoryName + '</p>\
-                                                <p>Name: ' + product.name + '</p>\
-                                                <p>Price: ' + Number(product.price).toFixed(2) + '</p>\
-                                                <button class="remove_request ' + i.toString() + '">Remove</button></li>');
-        }
+            $("ul.order_products").append('\
+					<li>\
+            			<div class="product_image">\
+                			<img src="./data/images/' + product.photo + '" />\
+           				</div>\
+                        <h3 class="icon-' + categoryTag + '">' + categoryName + '</h3>\
+                        <h1>' + product.name + '</h1>\
+            			<p class="price">' + Number(product.price).toFixed(2) + '$</p>\
+                        <a class="remove_request ' + i.toString() + '">Remove</a>\
+        				</li>');
+		}
+
     }
+
 }
 
 $(document).ready(function() {
-    $(document).on('click', "button.remove_request", function(e) {
+    $(document).on('click', "a.remove_request", function(e) {
         var requestId = $( this ).attr('class').split(' ')[1];
         var request = JSON.parse(sessionStorage.getItem(requestId));
         request[5] = -1;
