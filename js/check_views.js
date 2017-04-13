d0dbaf31871b118346de675bf683c21d38ebc7b0
Diogo Redin <diogo.redin@gmail.com>
2017-04-14 00:09:12 +0100

    Started First Version of the KeyPad

--- js/check_views.js
+++ js/check_views.js
@@ -4,6 +4,25 @@
 *
 */
 
-$(document).ready(function() { 
+$(document).ready(function() {
+
+    var count = 0;
+    $(document).on('click', ".digit", function(e) {
+
+        var num = ($(this).clone().children().remove().end().text());
+        if (count < 11) {
+            $("#output").append('<span>' + num.trim() + '</span>');
+            count++;
+        }
+
+        e.preventDefault();
+    });
+
+    $(document).on('click', ".clear", function(e) {
+        $('#output span:last-child').remove();
+        count--;
+
+        e.preventDefault();
+    });
 
 });
